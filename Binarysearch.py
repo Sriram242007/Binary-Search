f4215e07b7d5df17ea94b280f5a554bcfb9f1b0d
Sriram S <sriramsowri2007@gmail.com>
2026-06-19 14:29:48 +0530

    Add files via upload

---
+++ Binarysearch.py
@@ -0,0 +1,17 @@
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+
+    while left <= right:
+        mid = left + (right - left) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+arr=[1, 2, 3, 5, 8] 
+target=int(input("Enter the element to search: "))
+result = binary_search(arr, target)
+print(result)
